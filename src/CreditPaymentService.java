ebff22a8ff2ca3dbbc94587775a86774638f115e
Julia Rusanova <115406267+00Julie00@users.noreply.github.com>
2022-12-08 18:37:32 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,16 +1,8 @@
 public class CreditPaymentService {
-    double creditTerm;
-    double n = creditTerm * 12;
-
-    double interestRate;
-    double i = ((interestRate * 0.1) / 12);
-
-    double creditAmount;
-    double a = creditAmount;
-
     public double calculate(double i, double n, double a) {
-        double monthlyPayments = a * ((((1 + i) * n) * i) / (((1 + i) * n) - 1));
-        return monthlyPayments;
+        double interestRate = i / 100 / 12;
 
+        double monthlyPayments = a * (interestRate * Math.pow((1 + interestRate), n)) / (Math.pow((1 + interestRate), n) - 1);
+        return monthlyPayments;
     }
 }
