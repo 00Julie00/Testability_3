520d3b5febbdda8cd128d15d514a59b1110208b0
Julia Rusanova <115406267+00Julie00@users.noreply.github.com>
2022-12-08 18:38:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double creditAmount = 1_000_000;
-        double creditTerm = 1;
-        double interestRate = 9.99;
-        double monthlyRate = service.calculate(interestRate, creditTerm, creditAmount);
+        double a = 1_000_000;
+        double n = 1;
+        double i = 9.99;
+        double monthlyRate = service.calculate(i, n, a);
 
         System.out.println();
         System.out.println("Сумма кредита - 1 000 000 руб.\n Срок кредита - 1 год.\n Ставка - 9.99%");
-        System.out.println("Ежемесячный платеж -" + service.calculate(9.99, 1, 1_000_000));
+        System.out.println("Ежемесячный платеж -" + service.calculate(9.99, 12, 1_000_000));
 
         System.out.println();
         System.out.println("Сумма кредита - 1 000 000 руб.\n Срок кредита - 2 года.\n Ставка - 9.99%");
-        System.out.println("Ежемесячный платеж - " + service.calculate(9.99, 2, 1_000_000));
+        System.out.println("Ежемесячный платеж - " + service.calculate(9.99, 24, 1_000_000));
 
         System.out.println();
         System.out.println("Сумма кредита - 1 000 000 руб.\n Срок кредита - 3 года.\n Ставка - 9.99%");
-        System.out.println("Ежемесячный платеж - " + service.calculate(9.99, 3, 1_000_000));
+        System.out.println("Ежемесячный платеж - " + service.calculate(9.99, 36, 1_000_000));
     }
 }
